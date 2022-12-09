d0d05c70303cdcae7fe1f0b3e9083d5a35f2c121
MarcoGolan <marco@golan.cl>
2022-12-09 18:25:44 -0300

    Repository on consuming 'Rick and Morty' API initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import Navbar from './components/Navbar'
+
+const url = "https://rickandmortyapi.com/api/character"
+
+const getCharacterAPI = async (url) => {
+
+  const response = await fetch(url)
+  const data = await response.json()
+
+  console.log(data)
+}
 
 function App() {
+
+  useEffect(() => {
+    getCharacterAPI(url)
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar brand="Rick and Morty App!"/>
     </div>
   );
 }
